54576dd4a6b86ced8b8dc8e502bff123b71238c5
eiji03aero <eiji03aero@gmail.com>
2018-02-03 18:38:36 +0900

    finished 6.2

--- js/spa.model.js
+++ js/spa.model.js
@@ -269,10 +269,17 @@ spa.model = (function () {
           name: person_map.name
         };
 
+        if (chatee && chatee.id === make_person.id) {
+          is_chatee_online = true;
+        }
+
         makePerson(make_person_map);
       }
 
       stateMap.people_db.sort('name');
+      // Cancel the chatee if they are not online.
+      // As a result, [spa-setchatee] event will be published.
+      if (chatee && ! is_chatee_online) { set_chatee(''); }
     };
 
     _publish_listchange = function (arg_list) {
@@ -313,14 +320,57 @@ spa.model = (function () {
 
       sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
       sio.on('listchange', _publish_listchange);
-      sio.on('updatechat', _publish_chat);
+      sio.on('updatechat', _publish_updatechat);
       stateMap.is_connected = true;
       return true;
-    }
+    };
+
+    send_msg = function (msg_text) {
+      var msg_map,
+        sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+
+      if (!sio) { return false; }
+      if (!(stateMap.user && chatee)) { return false; }
+
+      msg_map = {
+        dest_id: chatee.id,
+        dest_name: chatee.name,
+        sender_id: stateMap.user.id,
+        msg_text: msg_text,
+      };
+
+      // Since [updatechat] is supposed to be published,
+      // it's okay to display message.
+      _publish_updatechat([msg_map]);
+      sio.emit('updatechat', msg_map);
+      return true;
+    };
+
+    set_chatee = function (person_id) {
+      var new_chatee;
+      new_chatee = stateMap.people_cid_map[person_id];
+      if (new_chatee) {
+        if (chatee && chatee.id === new_chatee.id) {
+          return false;
+        }
+      }
+      else {
+        new_chatee = null;
+      }
+
+      $.gevent.publish('spa-setchatee',
+        { old_chatee: chatee, new_chatee: new_chatee }
+      );
+      chatee = new_chatee;
+      return true;
+    };
 
     return {
       _leave: _leave_chat,
-      join: join_chat
+      get_chatee: get_chatee,
+      join: join_chat,
+      send_msg: send_msg,
+      set_chatee: set_chatee,
     };
   }());
 
